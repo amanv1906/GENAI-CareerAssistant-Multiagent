28b0813e65550389d0e207caeac760f17bf1e9ae
AMAN VARYANI <30382575+amanv1906@users.noreply.github.com>
2024-08-28 13:12:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -229,7 +229,7 @@ with input_section:
     with st.form(key="query_form", clear_on_submit=True):
         user_input_query = st.text_input(
             "Query:",
-            value=(selected_query if selected_query else "Layoff news of various tech giants and number of employees impacted?"),
+            value=(selected_query if selected_query else "Detail analysis of latest layoff news India?"),
             placeholder="📝 Write your query or select from the above",
             key="input",
         )
